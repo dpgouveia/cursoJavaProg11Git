c31fc024c794960657b4c0f6b6d6a7f3e94f527b
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-02-09 14:43:44 -0300

    Adicao de nova mensagem no metodo novo do pacote S12Git.Program

--- src/s12Git/Program.java
+++ src/s12Git/Program.java
@@ -10,39 +10,39 @@ public class Program {
 		Scanner input = new Scanner(System.in);
 
 		aula107_exemplo01(input);
-		
+
 		aula109_exemplo01(input);
-		
+
 		input.close();
 
 	}
-	
+
 	public static void aula109_exemplo01(Scanner input) {
 
 		Locale.setDefault(Locale.US);
 		input.reset();
-		
+
 		System.out.println("\n----------------------------");
 		System.out.println("AULA 109 - EXEMPLOE 01");
 		System.out.println("----------------------------\n");
-		
+
 		System.out.println("Bom dia!!!!");
 		System.out.println("Boa tarde!!!");
-		
+		System.out.println("Boa noite!!!");
+
 	}
-	
+
 	public static void aula107_exemplo01(Scanner input) {
 
 		Locale.setDefault(Locale.US);
 		input.reset();
-		
+
 		System.out.println("\n----------------------------");
 		System.out.println("AULA 107 - EXEMPLOE 01");
 		System.out.println("----------------------------\n");
-		
+
 		System.out.println("Bom dia!!!!");
-		
+
 	}
-	
 
 }
