00389596a70cbcb5b68425996a801d918664a951
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-02-09 01:22:54 -0300

    Ajuste no programa da secao 12 (nova funcao que apresenta os exemplos do professor durante o curso

--- src/s12Git/Program.java
+++ src/s12Git/Program.java
@@ -1,5 +1,6 @@
 package s12Git;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
@@ -8,10 +9,24 @@ public class Program {
 
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Hello World man!!!");
-
+		aula107_exemplo01(input);
+		
 		input.close();
 
 	}
+	
+	public static void aula107_exemplo01(Scanner input) {
+
+		Locale.setDefault(Locale.US);
+		input.reset();
+		
+		System.out.println("\n----------------------------");
+		System.out.println("AULA 107 - EXEMPLOE 01");
+		System.out.println("----------------------------\n");
+		
+		System.out.println("Bom dia!!!!");
+		
+	}
+	
 
 }
