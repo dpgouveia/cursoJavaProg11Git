a3fe571b90bc049833b16b3cf8019f3504126a54
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-03-09 01:26:44 -0300

    Ajuste no projeto de xadrez criando a classe Queen com todos os movimentos possiveis da rainha. Codigo desenvolvido ainda nao foi testado!

--- src/s16/ChessSystem/Chess/ChessMatch.java
+++ src/s16/ChessSystem/Chess/ChessMatch.java
@@ -13,6 +13,7 @@ import s16.ChessSystem.Chess.Pieces.Bishop;
 import s16.ChessSystem.Chess.Pieces.King;
 import s16.ChessSystem.Chess.Pieces.Knight;
 import s16.ChessSystem.Chess.Pieces.Pawn;
+import s16.ChessSystem.Chess.Pieces.Queen;
 import s16.ChessSystem.Chess.Pieces.Rook;
 
 public class ChessMatch {
@@ -259,11 +260,11 @@ public class ChessMatch {
 		this.placeNewPiece('a', 8, new Rook(this.board, Color.BLACK));
 		this.placeNewPiece('b', 8, new Knight(this.board, Color.BLACK));
 		this.placeNewPiece('c', 8, new Bishop(this.board, Color.BLACK));
+		this.placeNewPiece('d', 8, new Queen(this.board, Color.BLACK));
 		this.placeNewPiece('e', 8, new King(this.board, Color.BLACK));
 		this.placeNewPiece('f', 8, new Bishop(this.board, Color.BLACK));
 		this.placeNewPiece('g', 8, new Knight(this.board, Color.BLACK));
 		this.placeNewPiece('h', 8, new Rook(this.board, Color.BLACK));
-		
 		this.placeNewPiece('a', 7, new Pawn(this.board, Color.BLACK));
 		this.placeNewPiece('b', 7, new Pawn(this.board, Color.BLACK));
 		this.placeNewPiece('c', 7, new Pawn(this.board, Color.BLACK));
@@ -277,11 +278,11 @@ public class ChessMatch {
 		this.placeNewPiece('a', 1, new Rook(this.board, Color.WHITE));
 		this.placeNewPiece('b', 1, new Knight(this.board, Color.WHITE));
 		this.placeNewPiece('c', 1, new Bishop(this.board, Color.WHITE));
+		this.placeNewPiece('d', 1, new Queen(this.board, Color.WHITE));
 		this.placeNewPiece('e', 1, new King(this.board, Color.WHITE));
 		this.placeNewPiece('f', 1, new Bishop(this.board, Color.WHITE));
 		this.placeNewPiece('g', 1, new Knight(this.board, Color.WHITE));
 		this.placeNewPiece('h', 1, new Rook(this.board, Color.WHITE));
-		
 		this.placeNewPiece('a', 2, new Pawn(this.board, Color.WHITE));
 		this.placeNewPiece('b', 2, new Pawn(this.board, Color.WHITE));
 		this.placeNewPiece('c', 2, new Pawn(this.board, Color.WHITE));
---
+++ src/s16/ChessSystem/Chess/Pieces/Queen.java
@@ -0,0 +1,114 @@
+package s16.ChessSystem.Chess.Pieces;
+
+import s16.ChessSystem.BoardGame.Board;
+import s16.ChessSystem.BoardGame.Position;
+import s16.ChessSystem.Chess.ChessPiece;
+import s16.ChessSystem.Chess.Enum.Color;
+
+public class Queen extends ChessPiece {
+
+	// construtores
+	public Queen(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	// metodos
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean possibleMoves[][] = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+		Position evalMove = new Position(0, 0);
+
+		// N
+		evalMove.setValues(this.position.getRow() - 1, this.position.getColumn());
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow() - 1, evalMove.getColumn());
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// NW
+		evalMove.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow() - 1, evalMove.getColumn() + 1);
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// W
+		evalMove.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow(), evalMove.getColumn() + 1);
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// SW
+		evalMove.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow() - 1, evalMove.getColumn());
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// S
+		evalMove.setValues(this.position.getRow() + 1, this.position.getColumn());
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow() + 1, evalMove.getColumn());
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// SE
+		evalMove.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow() + 1, evalMove.getColumn() - 1);
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// E
+		evalMove.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow(), evalMove.getColumn() - 1);
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		// NE
+		evalMove.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		while (this.getBoard().positionExists(evalMove) && !this.getBoard().thereIsAPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+			evalMove.setValues(evalMove.getRow() + 1, evalMove.getColumn() - 1);
+		}
+		if (this.getBoard().positionExists(evalMove) && this.isThereOpponentPiece(evalMove)) {
+			possibleMoves[evalMove.getRow()][evalMove.getColumn()] = true;
+		}
+
+		return possibleMoves;
+	}
+
+	@Override
+	public String toString() {
+		if (this.getColor() == Color.WHITE) {
+			return "WQ";
+		} else {
+			return "BQ";
+		}
+	}
+
+}
