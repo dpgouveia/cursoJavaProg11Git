7697bd7800fc73a07a6eb0010d1dc473b44b6b39
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-04-11 01:29:01 -0300

    Adicao de codigo referente a aula 220 do capitulo 20

--- src/s20/ProgFuncionalLambda/application/Program.java
+++ src/s20/ProgFuncionalLambda/application/Program.java
@@ -65,7 +65,9 @@ public class Program {
 //		iniciarAula("218_exemplo_01", input);
 
 //		iniciarAula("219_exemplo_01", input);
-		iniciarAula("219_exemplo_02", input);
+//		iniciarAula("219_exemplo_02", input);
+		
+		iniciarAula("220_exemplo_01", input);
 
 		input.close();
 
@@ -741,6 +743,28 @@ public class Program {
 
 				break;
 			}
+			
+			case "220_exemplo_01": {
+
+				List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+				Stream<Integer> st1 = list.stream().map(x -> x * 10);
+				System.out.println();
+				System.out.println("------------------------------------------");
+				System.out.println(Arrays.toString(st1.toArray()));
+				
+				int sum = list.stream().reduce(0, (x, y) -> x + y);
+				System.out.println();
+				System.out.println("------------------------------------------");
+				System.out.println("Somatorio da stream: " + sum);
+				
+				List<Integer> newList = list.stream().filter(x -> x % 2 == 0).map(x -> x * 10).collect(Collectors.toList());
+				System.out.println();
+				System.out.println("------------------------------------------");
+				System.out.println("Somatorio dos numeros pares da lista multiplicados por 10: " + Arrays.toString(newList.toArray()));
+				
+				break;
+			}
+			
 
 			default: {
 				throw new ProgramException("Aula inexistente");
