f3e915bf4c9c6a42dbe5285f02a3efa5f91391d4
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-04-20 00:41:50 -0300

    Adicao de codigo referente a aula 265 do capitulo 23

--- src/s23/WorkshopJavaFXJdbc/application/Main.java
+++ src/s23/WorkshopJavaFXJdbc/application/Main.java
@@ -1,86 +1,51 @@
 package s23.WorkshopJavaFXJdbc.application;
 
+import java.io.IOException;
 import java.util.Locale;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import s23.WorkshopJavaFXJdbc.exception.MainException;
 
 public class Main extends Application {
 
 	public static void main(String args[]) {
-
+		
+		System.out.println();
+		System.out.println("------------------------------------------");
+		System.out.println("INICIO DO PROGRAMA.");
+		
 		Locale.setDefault(Locale.US);
-
 		launch(args);
+		
 	}
 
 	@Override
 	public void start(Stage stage) {
 
-		System.out.println();
-		System.out.println("------------------------------------------");
-		System.out.println("INICIO DO PROGRAMA.");
-
 		try {
 
-			iniciarAula("aula264_exemplo01", stage);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/MainView.fxml"));
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);
+			stage.setScene(mainScene);
+			stage.setTitle("Workshop JavaFX JDBC - DEMO");
+			stage.show();
 
-		} catch (MainException e) {
+		} catch (IOException | MainException e) {
 			System.out.println();
 			System.out.println("------------------------------------------");
-			System.out.println("ERRO NÃO TRATADO PELO PROGRAMA: " + e.getMessage());
+			System.out.println("ERRO DURANTE A EXECUÇÃO DO PROGRAMA!");
+			System.out.println("CAUSA: " + e.getMessage());
 			System.out.println("Encerrando programa....");
-		} catch (RuntimeException e) {
-			System.out.println();
-			System.out.println("------------------------------------------");
-			System.out.println("ERRO INESPERADO durante a execução do programa!");
-			System.out.println("Encerrando programa....");
-			e.printStackTrace();
-		} catch (Exception e) {
-			System.out.println();
-			System.out.println("------------------------------------------");
-			System.out.println("ERRO FATAL NA MAQUINA VIRTUAL JAVA durante a execução do programa!");
-			System.out.println("Encerrando programa....");
-			e.printStackTrace();
 		} finally {
 			System.out.println();
 			System.out.println("------------------------------------------");
 			System.out.println("FIM DO PROGRAMA.");
 		}
-
-	}
-
-	private void iniciarAula(String aula, Stage stage) {
-
-		System.out.println();
-		System.out.println("------------------------------------------");
-		System.out.println("INICIO AULA: " + aula);
-		System.out.println();
-
-		switch (aula) {
-
-		case "aula264_exemplo01": {
-
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
-//			scene.getStylesheets().add(this.getClass().getResource("application.css").toExternalForm());
-			stage.setScene(scene);
-			stage.show();
-
-			break;
-		}
-
-		default:
-			throw new MainException("Aula inexistente");
-
-		}
-
-		System.out.println();
-		System.out.println("------------------------------------------");
-		System.out.println("FIM AULA: " + aula);
 
 	}
 
