ac8c2041296c981bd6194756ad73c853b84df8af
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-04-26 15:39:22 -0300

    Ajuste no projeto para o demo do javafx rodar no notebook (ajuste no .nat somente)

--- build/runJar.bat
+++ build/runJar.bat
@@ -1,10 +1,10 @@
 @echo off
-set JAVA_HOME=C:\Users\Familia\Documents\Daniel\eclipse\jre
-REM set JAVA_HOME=C:\Users\BRDPG1\Documents\Programas\jdk\jdk-11.0.6
+REM set JAVA_HOME=C:\Users\Familia\Documents\Daniel\eclipse\jre
+set JAVA_HOME=C:\Users\BRDPG1\Documents\Programas\eclipse\jre
 REM set JAVA_HOME=%CD%\jre
 
-set PATH_TO_JAVAFX=C:\Users\Familia\Documents\Daniel\eclipse\java-libs\javafx-sdk\lib
-REM set PATH_TO_JAVAFX=%APP_CURRENT_PATH%\javafx-sdk\lib
+REM set PATH_TO_JAVAFX=C:\Users\Familia\Documents\Daniel\eclipse\java-libs\javafx-sdk\lib
+set PATH_TO_JAVAFX=C:\Users\BRDPG1\Documents\Programas\eclipse\java-libs\javafx-sdk\lib
 REM set PATH_TO_JAVAFX=%CD%\javafx-sdk\lib
 
 set PATH=%JAVA_HOME%\bin;%PATH%
