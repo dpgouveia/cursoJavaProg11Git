9de9d1c2fb5d35afb0c7863ca5bda592c5ae3eed
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-04-27 01:03:38 -0300

    Adicao de codigo referente a aula 303 do capitulo 25

--- ws-sts/ProjetoJPA/src/main/java/com/projetojpa/application/entities/User.java
+++ ws-sts/ProjetoJPA/src/main/java/com/projetojpa/application/entities/User.java
@@ -2,13 +2,18 @@ package com.projetojpa.application.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity public class User implements Serializable {
 
 	// atirbutos estáticos
 	private static final long serialVersionUID = 1L;
 
 	// atributos
-	private Integer id;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Integer id;
 	private String name;
 	private String email;
 	private String phone;
