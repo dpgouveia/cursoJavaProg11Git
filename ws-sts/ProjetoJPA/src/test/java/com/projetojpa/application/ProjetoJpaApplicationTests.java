dff288fa655cd1bf806ff768ca95629493576f8d
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-04-27 00:27:03 -0300

    Adicao do projeto inicial Spring Boot com JPA (tive baixar as dependencias manualmente no pom.xml) referente a aula 301 do capitulo 25

---
+++ ws-sts/ProjetoJPA/src/test/java/com/projetojpa/application/ProjetoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojpa.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
