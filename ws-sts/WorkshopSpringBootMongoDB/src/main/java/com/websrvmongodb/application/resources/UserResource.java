476f172ccc63dc1d41b118a9330789ddbbff020d
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-05-06 00:38:37 -0300

    Adição de código referente a aula 342 do capítulo 26

--- ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/resources/UserResource.java
+++ ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.websrvmongodb.application.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.websrvmongodb.application.domain.User;
+import com.websrvmongodb.application.dto.UserDTO;
 import com.websrvmongodb.application.services.UserService;
 
 @RestController
@@ -19,8 +20,9 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		return ResponseEntity.ok().body(service.findAll());
+	public ResponseEntity<List<UserDTO>> findAll() {
+		return ResponseEntity.ok()
+				.body(service.findAll().stream().map(user -> new UserDTO(user)).collect(Collectors.toList()));
 	}
 
 }
