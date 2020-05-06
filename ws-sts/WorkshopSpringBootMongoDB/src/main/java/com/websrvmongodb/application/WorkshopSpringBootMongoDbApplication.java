0c8c9aad6e0d5065bc65a4a561faff8546bcb53b
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-05-05 23:29:19 -0300

    Adicao do projeto inicial do mongodb com webservices do capitulo 26

---
+++ ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/WorkshopSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.websrvmongodb.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ ws-sts/WorkshopSpringBootMongoDB/src/test/java/com/websrvmongodb/application/WorkshopSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websrvmongodb.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
