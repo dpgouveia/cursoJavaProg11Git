0097ac2b35854d3f7017291bf237849a833bcdff
Daniel Gouveia <daniel.gouveia@gmail.com>
2020-05-07 21:54:46 -0300

    Adição de código referente a aula 350 do capítulo 26

--- ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/config/Instantiation.java
+++ ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/config/Instantiation.java
@@ -31,13 +31,17 @@ public class Instantiation implements CommandLineRunner {
 		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		userRepository.save(maria);
+		User bob = new User(null, "Bob Brown", "bob@gmail.com");
+		User alex = new User(null, "Alex Brown", "alex@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, bob, alex));
 
-		Post p1 = new Post(null, Instant.parse("2018-03-21T00:00:00.00Z"), "Partiu viagem",
-				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post p2 = new Post(null, Instant.parse("2018-03-23T00:00:00.00Z"), "Bom dia", "Acordei feliz hoje!",
-				new AuthorDTO(maria));
+		Post p1 = new Post(null, Instant.parse("2018-03-21T00:00:00.00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, Instant.parse("2018-03-23T00:00:00.00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(p1, p2));
+		
+		maria.getPosts().addAll(Arrays.asList(p1, p2));
+		userRepository.save(maria);
+		
 
 	}
 
--- ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/domain/User.java
+++ ws-sts/WorkshopSpringBootMongoDB/src/main/java/com/websrvmongodb/application/domain/User.java
@@ -1,8 +1,11 @@
 package com.websrvmongodb.application.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.websrvmongodb.application.dto.UserDTO;
@@ -16,7 +19,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
-
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -88,6 +94,10 @@ public class User implements Serializable {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 }
